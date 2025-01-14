1886ce70d25384ce24893c8637f75521917de1f2
mdrakib02z <mdrakibahmed515@gmail.comm>
2025-01-14 13:45:07 +0000

    Users admin added perfectly

--- src/Pages/Deshbord/DeshbordItems/AllUsers.jsx
+++ src/Pages/Deshbord/DeshbordItems/AllUsers.jsx
@@ -1,19 +1,65 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
-import { FaMoneyBillAlt } from "react-icons/fa";
+import { FaMoneyBillAlt, FaUser } from "react-icons/fa";
 import { MdOutlineDeleteOutline } from "react-icons/md";
-
+import Swal from "sweetalert2";
 
 export default function AllUsers() {
   const axiousSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiousSecure.get("users");
       return res.data;
     },
   });
+  // MAke Admin
+  const handleMakeAdmin = (user) => {
+    axiousSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} Is Admin Now!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiousSecure
+          .delete(`/users/${id}`)
+          .then((res) => {
+            refetch();
+            if (res.data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    });
+  };
+
   return (
     <section className=" px-4 py-6 md:py-8 lg:py-12">
       <div className="w-full bg-white p-6 md:p-8 lg:p-12">
@@ -52,13 +98,17 @@ export default function AllUsers() {
                       <td className="py-4 px-4">{index + 1}</td>
                       <td className="py-4 px-4 text-left">{user.name}</td>
                       <td className="py-4 px-4">${user.email}</td>
-                      <td className="py-4 px-4">${user?.role}</td>
+                     {user.role == 'admin'? "Admin" : <td className="py-4 px-4">
+                        <button onClick={() => handleMakeAdmin(user)}>
+                          <FaUser></FaUser>
+                        </button>
+                      </td>}
                       <td className="py-4 px-4">
                         <button
                           onClick={() => handleDelete(user._id)}
                           className="bg-black text-white px-6 py-2 hover:bg-orange-500 rounded  transition-colors duration-200"
                         >
-                        <MdOutlineDeleteOutline />
+                          <MdOutlineDeleteOutline />
                         </button>
                       </td>
                     </tr>
