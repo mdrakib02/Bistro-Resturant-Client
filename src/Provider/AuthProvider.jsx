af776f9124015825c29664f69dc470afc9e83cc1
mdrakib02z <mdrakibahmed515@gmail.comm>
2025-01-15 20:23:11 +0000

    Jwt add perfectly in the client side with conditionally

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -11,10 +11,12 @@ import {
 
 import AuthContext from "./Authcontext";
 import Auth from "../Firebase/Firebase.config";
+import UseAxiosPublic from "../Hooks/UseAxiosPublic";
 const googleProvider = new GoogleAuthProvider();
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const axiosPublic = UseAxiosPublic();
 
   const createUser = (email, password) => {
     setLoading(true);
@@ -46,7 +48,23 @@ export default function AuthProvider({ children }) {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(Auth, (currentUser) => {
       setUser(currentUser);
-      console.log("Current User", currentUser);
+      if (currentUser) {
+        const userInfo = { email: currentUser.email };
+        axiosPublic
+          .post("/jwt", userInfo)
+          .then((res) => {
+            console.log(res.data.token);
+            if (res.data.token) {
+              localStorage.setItem("access_token", res.data.token); // Use an underscore instead of a space
+            }
+          })
+          .catch((error) => {
+            console.error("Error fetching token:", error.message);
+            localStorage.removeItem("access_token"); // Ensure cleanup on error
+          });
+      } else {
+        localStorage.removeItem("access_token");
+      }
       setLoading(false);
     });
     return () => unsubscribe(); // Clean up
